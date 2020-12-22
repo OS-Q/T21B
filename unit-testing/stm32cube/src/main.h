3675870bb664e7af60cee3bfc754ff0f333eb304
qitas <qitas@qitas.cn>
2020-12-22 19:17:31 +0800

    fix

---
+++ unit-testing/stm32cube/src/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "stm32f4xx_hal.h"
+
+#define LED_PIN                                GPIO_PIN_13
+#define LED_GPIO_PORT                          GPIOC
+#define LED_GPIO_CLK_ENABLE()                  __HAL_RCC_GPIOC_CLK_ENABLE()
+
+#endif // MAIN_H
